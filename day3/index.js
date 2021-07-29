f735ec4b83292a235077e5cfea3ca632e87a056e
cedric <none@none.none>
2021-07-29 13:45:15 +0200

    Less water boiling code

--- day3/index.js
+++ day3/index.js
@@ -1,22 +1,23 @@
-const blurButton    = document.getElementById("blur");
-const spacingButton = document.getElementById("spacing");
-const baseColor     = document.getElementById("base");
-
-
-blurButton.addEventListener("input", (e) => {
+function deal_with_unite_button(e)
+{
     const unite = e.target.dataset.unite || "px";
     const value = e.target.value || 0;
-    document.documentElement.style.setProperty(`--${e.target.name}`, `${value}${unite}`)
+    document.documentElement.style.setProperty(`--${e.target.name}`, `${value}${unite}`);
+}
+
+const blurButton = document.getElementById("blur");
+blurButton.addEventListener("input", (e) => {
+    deal_with_unite_button(e);
 });
 
 
+const spacingButton = document.getElementById("spacing");
 spacingButton.addEventListener("input", (e) => {
-    const unite = e.target.dataset.unite || "px";
-    const value = e.target.value;
-    document.documentElement.style.setProperty(`--${e.target.name}`, `${value}${unite}`)
+    deal_with_unite_button(e);
 });
 
 
+const baseColor = document.getElementById("base");
 baseColor.addEventListener("input", (e) => {
-    document.documentElement.style.setProperty(`--${e.target.name}`, `${e.target.value}`)
+    document.documentElement.style.setProperty(`--${e.target.name}`, `${e.target.value}`);
 });
